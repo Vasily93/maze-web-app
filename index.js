63e518e809a1345db7613aa17b66bb596bb9ae7c
Vasily93 <markovavasily@gmail.com>
2021-06-26 16:57:25 -0700

    genarating grid and startes cell

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const width = 600;
 const height = 600;
+const cells = 3;
 
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body, //location of the canvas
     engine: engine,
-    option: {
+    options: {
+        wireframse: false,
         width,
         height
     }
@@ -17,12 +19,21 @@ Render.run(render);
 Runner.run(Runner.create(), engine);
 
 //Walls
-// (leftToRight , topToBottom , widht, height)
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
-    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-    Bodies.rectangle(800, 300, 40, 600, {isStatic: true}),
+    Bodies.rectangle((width/2), 0, width, 10, {isStatic: true}),
+    Bodies.rectangle((width/2), height, width, 10, {isStatic: true}),
+    Bodies.rectangle(0, (width/2), 10, height, {isStatic: true}),
+    Bodies.rectangle(width, (width/2), 10, height, {isStatic: true}),
 ];
 World.add(world, walls);
+
+//Grid Array generator
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const verticals = Array(cells).fill(null).map(() => Array(cells -1).fill(false));
+const horizontals = Array(cells).fill(null).map(() => Array(cells -1).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+console.log(startColumn, startRow);
 
