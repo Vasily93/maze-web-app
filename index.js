e73912806ff8a870051dcdb96a2ab2c92e59bbbb
Vasily93 <markovavasily@gmail.com>
2021-06-26 16:08:13 -0700

    statrign over with a blank canvas module

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
-    element: document.body,
+    element: document.body, //location of the canvas
     engine: engine,
     option: {
-        width: 800,
-        height: 600
+        width,
+        height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
-})
-World.add(world, shape);
+//Walls
+// (leftToRight , topToBottom , widht, height)
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
+    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
+    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
+    Bodies.rectangle(800, 300, 40, 600, {isStatic: true}),
+];
+World.add(world, walls);
+
