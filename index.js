987e3984d57b0e1598a628c66852f2816a1a27e0
Vasily93 <markovavasily@gmail.com>
2021-06-28 12:37:52 -0700

    finshed recursive funciton to craete a maze arrays hor and ver

--- index.js
+++ index.js
@@ -28,12 +28,63 @@ const walls = [
 World.add(world, walls);
 
 //Grid Array generator
+const shuffle = (arr) => { //shuffling an array of neighbor cells
+    let counter = arr.length;
+    
+    while(counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+    }
+    return arr;
+}
+
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
 const verticals = Array(cells).fill(null).map(() => Array(cells -1).fill(false));
-const horizontals = Array(cells).fill(null).map(() => Array(cells -1).fill(false));
+const horizontals = Array(cells -1).fill(null).map(() => Array(cells).fill(false));
 
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
 
-console.log(startColumn, startRow);
+const stepThroughCell = (row, column) => { //funciton to create a maze
+    if(grid[row][column]) { //check if the cell was already visited
+        return;
+    }
+    grid[row][column] = true;
+    
+    const neighbors = shuffle([ //getting all naighbors of the current cell
+        [row-1, column, 'up'],
+        [row, column+1, 'rihgt'],
+        [row+1, column, 'down'],
+        [row, column-1, 'left']
+    ]);
+    for(let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
+
+        if( //check if neighbor exist and skip the step if not
+            nextRow < 0 ||
+            nextRow >= cells ||
+            nextColumn < 0 ||
+            nextColumn >= cells 
+        ) { continue;}
+        if(grid[nextRow][nextColumn]) { //check if we alredy visited that neighbor
+            continue;
+        }
+        if(direction === 'left') {//change a chosen border to true by direction of current neighbor
+            verticals[row][column - 1] = true;
+        } else if(direction === 'right') {
+            verticals[row][column] = true;
+        } else if(direction === 'up') {
+            horizontals[row - 1][column] = true;
+        } else if(direction === 'down') {
+            horizontals[row][column] = true;
+        }
+        stepThroughCell(nextRow, nextColumn); //function call for recursive behavior
+    }
+};
+stepThroughCell(startRow, startColumn);
+
 
